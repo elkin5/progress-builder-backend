803a47542607f0a3cbe204f8f6ba143a809f08c8
Elkin Hurtado <syselkin5@gmail.com>
2024-09-14 00:05:24 -0500

    Crear variaables de entorno para despliegue

--- src/database/sequelize.js
+++ src/database/sequelize.js
@@ -1,12 +1,28 @@
-// sequelize.js
 const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+
+// Cargar el archivo de entorno según el valor de NODE_ENV
+const envFile = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env';
+dotenv.config({ path: envFile });
 
 // Configuración de conexión a la base de datos PostgreSQL
-const sequelize = new Sequelize('progressbuilder', 'admin', '123', {
-    host: 'localhost',
-    dialect: 'postgres', // Tipo de base de datos
-    logging: false, // Desactiva el log de consultas SQL en la consola
-});
+// const sequelize = new Sequelize('progressbuilder', 'admin', '123', {
+//     host: 'localhost',
+//     dialect: 'postgres', // Tipo de base de datos
+//     logging: false, // Desactiva el log de consultas SQL en la consola
+// });
+// Configura la conexión con la base de datos usando variables de entorno
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        port: process.env.DB_PORT,
+        logging: process.env.NODE_ENV === 'development',
+    }
+);
 
 async function testConnection() {
     try {
